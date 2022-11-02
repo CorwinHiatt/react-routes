87b801b508d89697d91fb2d913b677d6677437dd
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-02 10:20:49 -0400

    adding features/pages/routes

---
+++ src/scenes/AddAlbum.jsx
@@ -0,0 +1,10 @@
+
+
+export default function AddAlbum(){
+    return(
+        <>
+        <h1>Add Album Title HERE ....</h1>
+        <p> new Album List here</p>
+        </>
+    )
+}
---
+++ src/scenes/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <>
+        <h1>HEADER GOES HERE ....</h1>
+        <p>Album List here</p>
+        </>
+    )
+}
